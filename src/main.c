d94ce1734e8851be59aecc19e4cb1b7cb4f7e1d5
jpkerkhoff-dev <jp@DESKTOP-GCG26HV.localdomain>
2026-09-18 00:59:44 -0300

    whileloop test

--- src/main.c
+++ src/main.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 typedef struct{
-	char *b;
-	int len;
+  char *b;
+  int len;
 }AppBuffer;
 
 #define ABUF_INIT {NULL,0}
 
-void AbAppend(AppBuffer *ab, const char *s, int s_len){
-	
-}
+//void AbAppend(AppBuffer *ab, const char *s, int s_len){}
 
-void abFree(){
+//void abFree(){}
 
-}
-
-
-struct termios orsettings_term;
-
-// 1. Declare prototype first so enable_rawmode knows disable_rawmode exists
-void disable_rawmode(void);
+struct termios or_term;
 
 void disable_rawmode(void) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orsettings_term);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &or_term);
 }
 
 void enable_rawmode(void) {
-    if (tcgetattr(STDIN_FILENO, &orsettings_term) == -1) {
+    if (tcgetattr(STDIN_FILENO, &or_term) == -1) {
         perror("tcgetattr fail");
         exit(1);
     }
 
     atexit(disable_rawmode);
 
-    struct termios raw = orsettings_term;
+    struct termios raw = or_term;
 
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
@@ -46,17 +39,30 @@ void enable_rawmode(void) {
     raw.c_cc[VTIME] = 1;  // Wait up to 100ms before returning 0
 
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
-        perror("tcsetattr fail (new attributes)");
+        perror("tcsetattr fail (on raw)");
         exit(1);
     }
 }
 
 int main(void) {
-    enable_rawmode();
+  enable_rawmode();
 
-    while (1) {
-        
+  while (1) {
+    char c ='\0';
+
+    if(read(STDIN_FILENO, &c,1) == 1){
+      if(iscntrl(c)){
+        printf("CTRL + %c\r\n",c+64);
+      }
+      if(c == 17){
+        printf("Quitting...\r\n"); 
+        break;
+      }
+      else{
+        printf("%c\r\n",c);
+      }
     }
+  }
 
-    return 0;
+return 0;
 }
