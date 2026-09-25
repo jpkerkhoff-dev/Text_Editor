331184ba7217a06fdec489d2bbb998b75cf82c26
jpkerkhoff-dev <jp@DESKTOP-GCG26HV.localdomain>
2026-09-24 23:28:38 -0300

    Test1

--- src/main.c
+++ src/main.c
@@ -4,23 +4,19 @@
 #include <termios.h>
 #include <unistd.h>
 
-typedef struct{
-  char *b;
-  int len;
-}AppBuffer;
+typedef struct {
+    char *b;
+    int len;
+} AppBuffer;
 
-enum Key_Binds{
-    Arrow_Up,
-    Arrow_Down,
-    Arrow_Left,
-    Arrow_Right
+enum Key_Binds {
+    ARR_UP = 1000,
+    ARR_DO,
+    ARR_LE,
+    ARR_RI
 };
 
-#define ABUF_INIT {NULL,0}
-
-//void AbAppend(AppBuffer *ab, const char *s, int s_len){}
-
-//void abFree(){}
+#define ABUF_INIT {NULL, 0}
 
 struct termios or_term;
 
@@ -42,8 +38,13 @@ void Enable_Rawmode(void) {
     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
     raw.c_oflag &= ~(OPOST);
 
-    raw.c_cc[VMIN] = 0;   // Return immediately if no bytes available
-    raw.c_cc[VTIME] = 1;  // Wait up to 100ms before returning 0
+    /*
+     * Wait until at least one byte is available.
+     * This prevents read() from returning 0 after 100 ms
+     * and immediately closing the program.
+     */
+    raw.c_cc[VMIN] = 1;
+    raw.c_cc[VTIME] = 0;
 
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
         perror("tcsetattr fail (on raw)");
@@ -51,58 +52,123 @@ void Enable_Rawmode(void) {
     }
 }
 
-int Editor_Read_Key(void){
+int Interpret_Key(void) {
     char c;
 
-    while(read(STDIN_FILENO,&c,1) != 1){
-        if(c == '\x1b'){
-           char next[2];
-
-           if(read(STDIN_FILENO,&next[0],1) !=1){
-                return '\x1b';
-           }
-           if(read(STDIN_FILENO,&next[1],1)!=1){
-                return '\x1b';
-           }
-           if(next[0] == '['){
-                switch(next[1]){
-                    case 'A': return "Arrow_Up";         
-                    case 'B': return "Arrow_Down";
-                    case 'C': return "Arrow_Right";
-                    case 'D': return "Arrow_Left";
-                }
-           }
-           return '\x1b';
+    /*
+     * With VMIN = 1 this normally blocks until
+     * a character is available.
+     */
+    while (read(STDIN_FILENO, &c, 1) != 1) {
     }
-  }
-    return c;
+
+    /*
+     * Escape sequences are used by arrow keys.
+     */
+    if (c == '\x1b') {
+        char seq[2];
+
+        /*
+         * Temporarily use a timeout so pressing ESC by itself
+         * does not block forever waiting for more characters.
+         */
+        struct termios raw;
+
+        if (tcgetattr(STDIN_FILENO, &raw) == -1) {
+            return '\x1b';
+        }
+
+        raw.c_cc[VMIN] = 0;
+        raw.c_cc[VTIME] = 1;
+
+        if (tcsetattr(STDIN_FILENO, TCSANOW, &raw) == -1) {
+            return '\x1b';
+        }
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1 ||
+            read(STDIN_FILENO, &seq[1], 1) != 1) {
+
+            raw.c_cc[VMIN] = 1;
+            raw.c_cc[VTIME] = 0;
+            tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+
+            return '\x1b';
+        }
+
+        /*
+         * Restore normal blocking raw-mode reads.
+         */
+        raw.c_cc[VMIN] = 1;
+        raw.c_cc[VTIME] = 0;
+        tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A':
+                    return ARR_UP;
+
+                case 'B':
+                    return ARR_DO;
+
+                case 'C':
+                    return ARR_RI;
+
+                case 'D':
+                    return ARR_LE;
+            }
+        }
+
+        return '\x1b';
+    }
+
+    return (unsigned char)c;
 }
 
-int Editor_Interpret_Key(void){
-      int key = Editor_Read_Key;
+void Read_Key(void) {
+    while (1) {
+        int key = Interpret_Key();
 
-      switch(key){
-        case Arrow_Up:
-          cursor_y--;
-          break;
-        case Arrow_Down:
-          cursor_y++;
-          break;
-        case Arrow_Right:
-          cursor_x++;
-          break;
-        case Arrow_Left:
-          cursor_x--;
-          break;
-      }
+        /*
+         * Ctrl+Q = ASCII 17
+         */
+        if (key == 17) {
+            printf("Quitting...\r\n");
+            break;
+        }
+
+        switch (key) {
+            case ARR_UP:
+                printf("Arrow Up\r\n");
+                break;
+
+            case ARR_DO:
+                printf("Arrow Down\r\n");
+                break;
+
+            case ARR_LE:
+                printf("Arrow Left\r\n");
+                break;
+
+            case ARR_RI:
+                printf("Arrow Right\r\n");
+                break;
+
+            default:
+                if (key >= 1 && key <= 26) {
+                    printf("CTRL + %c\r\n", 'A' + key - 1);
+                } else if (iscntrl((unsigned char)key)) {
+                    printf("Control character ASCII: %d\r\n", key);
+                } else {
+                    printf("%c ASCII: %d\r\n", key, key);
+                }
+                break;
+        }
+    }
 }
 
 int main(void) {
-  Enable_Rawmode();
+    Enable_Rawmode();
+    Read_Key();
 
-  while (1) {
-    
-  }
-
-return 0;
+    return 0;
 }
